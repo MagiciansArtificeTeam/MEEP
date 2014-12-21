f84224e35cb3b1694078eb39908df5dc4989b4ef
poppypoppop <jimbo@exemail.com.au>
2014-12-21 16:07:38 +1000

    Setup

---
+++ src/com/meep/MEEP.java
@@ -0,0 +1,29 @@
+package com.meep;
+
+import com.meep.core.libs.ModInfo;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by James on 21/12/2014.
+ */
+@Mod(name = ModInfo.NAME, modid = ModInfo.MODID, version = ModInfo.VERSION)
+public class MEEP {
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ src/com/meep/core/libs/ModInfo.java
@@ -0,0 +1,12 @@
+package com.meep.core.libs;
+
+/**
+ * Created by James on 21/12/2014.
+ */
+public class ModInfo {
+    public static final String MODID = "meep";
+    public static final String NAME = "Minecraft Extra Expansion Pack";
+    public static final String VERSION = "1.0";
+    public static final String CLIENT = "com.meep.core.proxies.ClientProxy";
+    public static final String SERVER = "com.meep.core.proxies.CommonProxy";
+}
