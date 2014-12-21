560cd8f1673ef791a4d4087a4e54b56870787f69
poppypoppop <jimbo@exemail.com.au>
2014-12-21 16:45:06 +1000

    I SAID NO OVERRIDES BRAIN

--- src/com/meep/core/client/guis/GuiHandler.java
+++ src/com/meep/core/client/guis/GuiHandler.java
@@ -1,7 +1,19 @@
 package com.meep.core.client.guis;
 
+import cpw.mods.fml.common.network.IGuiHandler;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
 /**
  * Created by James on 21/12/2014.
  */
-public class GuiHandler {
+public class GuiHandler implements IGuiHandler{
+
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+    
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
 }
